54fa1c3db50ee64bd7b4c6350a5bb424f4fb2335
sidhunavjot <sidhunavjot001@gmail.com>
2024-05-25 22:37:59 +0530

    json

--- 5_pandasreajson.py
+++ 5_pandasreajson.py
@@ -5,6 +5,34 @@ import pandas as pd
 x = pd.read_json('sample4.json')
 print(x.to_string()) 
 
+# make json in data frame
 
+import pandas as pd
+x = {"people" : [
+    {
+       "firstName": "Joe",
+       "lastName": "Jackson",
+       "gender": "male",
+       "age": 28,
+       "number": "7349282382"
+    },
+    {
+       "firstName": "James",
+       "lastName": "Smith",
+       "gender": "male",
+       "age": 32,
+       "number": "5678568567"
+    },
+    {
+       "firstName": "Emily",
+       "lastName": "Jones",
+       "gender": "female",
+       "age": 24,
+       "number": "456754675"
+    }
+  ]}
+
+y = pd.DataFrame(x)
+print(y.to_string())
 
 
